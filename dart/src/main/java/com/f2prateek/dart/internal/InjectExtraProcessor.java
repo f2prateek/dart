0b2b6b2069d3db5b44792adf576cf83f9fe21c7a
Sun <yaming116@163.com>
2014-08-20 15:28:50 +0800

    Update InjectExtraProcessor.java

--- dart/src/main/java/com/f2prateek/dart/internal/InjectExtraProcessor.java
+++ dart/src/main/java/com/f2prateek/dart/internal/InjectExtraProcessor.java
@@ -233,10 +233,10 @@ public final class InjectExtraProcessor extends AbstractProcessor {
   * @return true if str is null or zero length
   */
   public static boolean isEmpty(String str) {
-    if (str == null || str.length() == 0)
+    if (str == null || str.length() == 0){
       return true;
-    else
-      return false;
+    }
+    return false;
    }
 
   private boolean containsTypeMirror(Collection<TypeMirror> mirrors, TypeMirror query) {
