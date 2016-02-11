0119565a98e5d5e02c7d3d6bc4a4f32c39a75d73
Daniel Molinero Reguera <dmolinero@groupon.com>
2016-02-11 20:03:09 +0000

    Henson error - linkage

--- dart-sample/src/main/java/com/f2prateek/dart/example/MainActivity.java
+++ dart-sample/src/main/java/com/f2prateek/dart/example/MainActivity.java
@@ -23,6 +23,8 @@ import android.os.Bundle;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import static com.f2prateek.dart.example.SampleActivity.UNEXISTING;
+
 public class MainActivity extends Activity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
