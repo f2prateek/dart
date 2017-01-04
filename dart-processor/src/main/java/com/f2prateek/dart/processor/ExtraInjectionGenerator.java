906788bfd91dc475c069e507a32576f854896c30
Wavesonics <adamwbrown@gmail.com>
2017-01-04 12:47:03 -0800

    Added missing import

--- dart-processor/src/main/java/com/f2prateek/dart/processor/ExtraInjectionGenerator.java
+++ dart-processor/src/main/java/com/f2prateek/dart/processor/ExtraInjectionGenerator.java
@@ -27,6 +27,7 @@ import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
+import com.squareup.javapoet.AnnotationSpec;
 import java.util.Collection;
 import java.util.List;
 import javax.lang.model.element.Modifier;
