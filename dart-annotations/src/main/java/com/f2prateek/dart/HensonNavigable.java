8dc741311195e34cef6f0c1a697bfee202c74c1b
Stéphane Nicolas <steff.nicolas@gmail.com>
2017-11-06 17:42:08 -0500

    Change annotations retention in version 2

--- dart-annotations/src/main/java/com/f2prateek/dart/HensonNavigable.java
+++ dart-annotations/src/main/java/com/f2prateek/dart/HensonNavigable.java
@@ -4,9 +4,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.SOURCE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 
-@Retention(SOURCE)
+@Retention(CLASS)
 @Target(TYPE)
 public @interface HensonNavigable {
     Class<?> model() default Void.class;
